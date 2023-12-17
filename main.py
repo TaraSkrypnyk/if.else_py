d81afe83eac794e95d60b783f78cc3c9d85e5c9e
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-17 17:09:20 +0200

    Ex7 was done

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 try:
     a = int(1)
-    res = str()
     b = -1
     while b <= 0:
         b = int(input("Введіть число "))
     s = str(input("Введіть символ "))
     for i in range(a, b + 1):
-        res += (s)
+        print(s, end='')
         a += 1
-    print(res)
 except Exception as e:
     print(e)

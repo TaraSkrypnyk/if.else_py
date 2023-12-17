ceb02b932862138f031c1d168ba1c8a17d538440
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-17 16:16:24 +0200

    Ex1 was done

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 try:
+    a= int(input("Введіть початок діапазону "))
+    b = int(input("Введіть і діапазону "))
+    if a>b:
+        a,b=b,a
+    for i in range(a,b):
+        if a%7==0:
+            print(a)
+        a+=1
 
 except Exception as e:
     print(e)

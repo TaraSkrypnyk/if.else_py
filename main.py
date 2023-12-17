4df4b35085ef10c710c25841e82d04871f48243e
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-17 16:53:23 +0200

    Ex5 was done

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 try:
-
+    a = int(1)
+    res = int(1)
+    b = -1
+    while b<0:
+        b = int(input("Введіть число "))
+    if b==0:
+        print("Факторіал вашого числа дорівнює 1")
+    else:
+         for i in range(a, b+1):
+             res *= a
+             a += 1
+         print ("Факторіал вашого числа дорівнює " + str(res))
 except Exception as e:
     print(e)

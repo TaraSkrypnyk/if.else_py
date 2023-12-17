fc73a4ea29a9225a5d1acd2c4d209dc970c87068
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-17 16:34:32 +0200

    Ex3 was done

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 try:
-
+    a = int(input("Введіть початок діапазону "))
+    b = int(input("Введіть і діапазону "))
+    if a > b:
+        a, b = b, a
+    for i in range(a, b):
+        if a % 15 == 0:
+            print("Fizz Buzz")
+        elif a % 5 == 0:
+            print("Buzz")
+        elif a % 3 == 0:
+            print("Fizz")
+        else:
+            print (a)
+        a += 1
 except Exception as e:
     print(e)

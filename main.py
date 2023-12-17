76c418bfffd0022b8d5a62d1a06a656d6308f276
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-17 20:51:10 +0200

    Ex8 was done

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 try:
-    a = -1
+    import random
+
+    symbol_list = [0, 1, 2, 3]
+    dig_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    upp_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                'V', 'W', 'X', 'Y', 'Z']
+    low_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+    sign_list = ['@', '#', ',', '?', '*', '.', '%']
     dig = 3
     low = 3
     upp = 3
     sign = 3
-    while a <= 0:
-        a = int(input("Введіть довжину пароля "))
+    b =-1
+    password=str()
+    while b <= 0:
+        b = int(input("Введіть довжину пароля "))
     while dig < 0 or dig>1:
         dig = float(input("Якщо ви хочете щоб в вашому паролі були цифри натисніть 1, інакше натисніть 0 "))
     while low < 0 or low > 1:
@@ -14,19 +24,30 @@ try:
         upp = float(input("Якщо ви хочете щоб в вашому паролі були еликі літери натисніть 1, інакше натисніть 0 "))
     while sign < 0 or sign > 1:
         sign = float(input("Якщо ви хочете щоб в вашому паролі були пеціальні символи 1, інакше натисніть 0 "))
-
-    import random
-    dig_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    upp_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    low_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    sign_list= ['@', '#', ',', '?', '*', '.', '%']
-    print("Вибір випадкової великої літери - ", random.choice(upp_list))
-    print("Вибір випадкової маленької літери - ", random.choice(low_list))
-    # if a > b:
-    #     a, b = b, a
-    # for i in range(a, b):
-    #     if a % 7 == 0:
-    #         print(a)
-    #     # a += 1
+    x = int(0)
+    i = int(0)
+    while (i<b):
+        r = random.choice(symbol_list)
+        if r==0:
+            if dig==1:
+                d = random.choice(dig_list)
+                password += d
+                i += 1
+        elif r==1:
+            if low==1:
+                l = random.choice(low_list)
+                password += l
+                i += 1
+        elif r==2:
+            if upp==1:
+                u = random.choice(upp_list)
+                password += u
+                i += 1
+        elif r==3:
+            if sign==1:
+                s = random.choice(sign_list)
+                password += s
+                i += 1
+    print("Ваш пароль: " + str(password))
 except Exception as e:
     print(e)

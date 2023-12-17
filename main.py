03d86190c24fd1c99e3dd75086b8446a6be8748b
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-17 16:58:48 +0200

    Ex6 was done

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 try:
-
+    a = int(1)
+    res = str()
+    b = -1
+    while b <= 0:
+        b = int(input("Введіть число "))
+    for i in range(a, b+1):
+        res += ("*")
+        a += 1
+    print (res)
 except Exception as e:
     print(e)
